7f7506148eeca3497680fc6a6af405b33e137ae1
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-03-29 07:25:09 -0300

    Staging de tudo feito até então

---
+++ calcularMedia.js
@@ -0,0 +1,47 @@
+const MINGRADE = 2.5
+
+function iterateOverCell(aluno){
+    let notasAluno = [...aluno.children].slice(1, 4).map(node => {
+        const nodeValue = Number(node.textContent)
+        if(nodeValue < MINGRADE){
+            markCellAsReproved(node)
+        }
+        return nodeValue
+    }).map(nota =>{
+        const result = Number(nota)
+        return result
+    })
+    let media = avgOf(notasAluno)
+    return media
+}
+
+function setMediaAlunos(){
+    let alunos = [...document.querySelectorAll('tr')]
+    alunos.slice(1).map(aluno => {
+        let media = iterateOverCell(aluno)
+        let mediaContainer = aluno.children[4]
+        mediaContainer.textContent = media
+        markStudentsAsReproved(media, mediaContainer)
+    })
+}
+
+function markStudentsAsReproved(currentGrade, reprovedContainer){
+    if(currentGrade < MINGRADE){
+        markCellAsReproved(reprovedContainer)
+    }
+}
+
+function markCellAsReproved(element){
+    element.classList.add('reprovado')
+}
+
+function avgOf(arr) {
+    return arr.reduce((acc, val) => acc+val)/arr.length
+}
+
+function main(){
+    setMediaAlunos()
+}
+
+main()
+
---
+++ inserirAluno.js
@@ -0,0 +1,53 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', handleAddAluno)
+
+
+function createCell(val){
+    const cell = document.createElement('td')
+    cell.textContent = val
+    return cell
+}
+
+
+function handleAddAluno(e){
+    e.preventDefault()
+    const inputNome = document.querySelector('#nome')
+    const inputNota1 = document.querySelector('#nota1')
+    const inputNota2 = document.querySelector('#nota2')
+    const inputNota3 = document.querySelector('#nota3')
+
+    const nome = inputNome.value
+    const nota1 = inputNota1.value
+    const nota2 = inputNota2.value
+    const nota3 = inputNota3.value
+
+    adicionarAluno(nome, nota1, nota2, nota3)
+
+
+    inputNome.value = ''
+    inputNota1.value = ''
+    inputNota2.value = ''
+    inputNota3.value = ''
+}
+
+
+function adicionarAluno(nome, nota1, nota2, nota3){
+    const TARGET = document.querySelector('table')
+    const row = document.createElement('tr')
+
+    const td1 = createCell(nome)
+    const td2 = createCell(nota1)
+    const td3 = createCell(nota2)
+    const td4 = createCell(nota3)
+    const td5 = createCell(0)
+    
+    row.appendChild(td1)
+    row.appendChild(td2)
+    row.appendChild(td3)
+    row.appendChild(td4)
+    row.appendChild(td5)
+
+    TARGET.appendChild(row)
+    setMediaAlunos()
+}
