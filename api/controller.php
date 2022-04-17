9384088681126177185a440fee0e2c0cceb00056
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2022-04-17 14:24:07 -0300

    Removidos arquivos e linhas de código desnecessários

--- api/controller.php
+++ api/controller.php
@@ -63,13 +63,4 @@ class Controller {
 
 }
 
-$aluno1 = new Aluno("Criosvaldo Daniel", 4, 5, 9);
-$aluno2 = new Aluno("Mary", 10, 9, 9);
-
-$listaAlunos = [$aluno1, $aluno2, $aluno1];
-
-$json = json_encode($listaAlunos);
-var_dump($json);
-
-
 ?>
