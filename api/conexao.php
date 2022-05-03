9c54528ef639cb89d3eafeaad68f924865629acf
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-03 08:10:54 -0300

    Senha correta inserida no código

--- api/conexao.php
+++ api/conexao.php
@@ -1,6 +1,6 @@
 <?php
     function create_connection(){
-        $conn = new PDO("mysql:host=localhost;dbname=bdalunos", "gabirel");
+        $conn = new PDO("mysql:host=fdb25.awardspace.net;dbname=3451205_crud", "3451205_crud", "KL@Peters060504");
         return $conn;
     }
 ?>
